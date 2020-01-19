9cb8ff0fff87fc66b012908e01ac8a9b40975cb2
Klaudia <44114240+klaudiaf24@users.noreply.github.com>
2020-01-19 23:37:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/main.js"></script>
     <title>Kartoteka sądownicza</title>
 </head>
 
